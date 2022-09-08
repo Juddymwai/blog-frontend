e4eaf14eeffd508017baa65643b978d5c8a36b46
Judy Mwai <juddiemukami@gmail.com>
2022-09-09 02:32:58 +0300

    first commit

---
+++ blog-client/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer(){
+    return (
+        <div style={{padding: 40+"px", backgroundColor:"black", color:"white"}}>
+            <h6 style={{textAlign:"center"}}>Designed By Judy Mwai</h6>
+        </div>
+    )
+}
+export default Footer;
+
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:Juddymwai/blog-frontend.git
+git push -u origin main
