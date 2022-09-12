256d2d511d2dfc32bf54752afa5e21701ce36f9b
Judy Mwai <juddiemukami@gmail.com>
2022-09-12 22:00:48 +0300

    Updates

---
+++ precache-manifest.2c2c90592a1c1e1ebca08968b8de4cba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2afedd17ff390af641de",
+    "url": "/static/css/main.864f0738.chunk.css"
+  },
+  {
+    "revision": "2afedd17ff390af641de",
+    "url": "/static/js/main.2afedd17.chunk.js"
+  },
+  {
+    "revision": "eaa02127bc6494e09f89",
+    "url": "/static/js/1.eaa02127.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ab387da583c080221f88fc6c54fd213c",
+    "url": "/index.html"
+  }
+];
