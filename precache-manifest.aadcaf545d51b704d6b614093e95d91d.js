635b7603e8e65092e5e1ddc82b86036c306509a7
Judy Mwai <juddiemukami@gmail.com>
2022-09-12 22:54:12 +0300

    Updates

---
+++ precache-manifest.aadcaf545d51b704d6b614093e95d91d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "596294b36442fbdae350",
+    "url": "/blog-frontend/static/css/main.05625d05.chunk.css"
+  },
+  {
+    "revision": "596294b36442fbdae350",
+    "url": "/blog-frontend/static/js/main.596294b3.chunk.js"
+  },
+  {
+    "revision": "0f61e678fb91ab668362",
+    "url": "/blog-frontend/static/js/1.0f61e678.chunk.js"
+  },
+  {
+    "revision": "a613dd5e51e7dcf6953a",
+    "url": "/blog-frontend/static/js/runtime~main.a613dd5e.js"
+  },
+  {
+    "revision": "35ab42b794e16a018ab2b41db6601e5d",
+    "url": "/blog-frontend/index.html"
+  }
+];
